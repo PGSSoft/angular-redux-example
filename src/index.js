66403aeddc5939c587a0337da4531b7f4ae880a8
Anna Konopka <ania@zdwola.pl>
2016-10-06 09:18:06 +0200

    Get rid of App Controller

--- src/index.js
+++ src/index.js
@@ -5,13 +5,10 @@ import productsInfo from './redux/reducers';
 
 import productsListModule from './components/products-list/products-list.module.js';
 
-function AppController() {}
-
 angular.module('app.starter', [ngRedux, productsListModule])
   .config(($ngReduxProvider) => {
     const reducer = combineReducers({ productsInfo });
     $ngReduxProvider.createStoreWith(reducer);
-  })
-  .controller('AppController', AppController);
+  });
 
 angular.bootstrap(document, ['app.starter']);
